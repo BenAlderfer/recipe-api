0bdd50210ac7afda165b567cba80546d24d87971
Ben Alderfer <benalderfer974@gmail.com>
2021-10-16 19:44:46 -0400

    Add missing fields to the model

--- models/recipe.js
+++ models/recipe.js
@@ -3,4 +3,8 @@ const Recipe = sequelize.define('recipe', {
     name: { type: Sequelize.STRING, allowNull: false },
     description: { type: Sequelize.STRING },
     lastUpdated: { type: Sequelize.DATE, defaultValue: Sequelize.NOW },
+    ingredients: { type: Sequelize.STRING, allowNull: false }, // markdown formatted string
+    instructions: { type: Sequelize.STRING }, // markdown formatted string
+    tools: { type: Sequelize.STRING }, // tools needed to make the recipe, markdown formatted string
+    image: { type: Sequelize.STRING }, // base64 encoded image
 });
